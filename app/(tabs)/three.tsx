37e3a1a8f7609882fb0239749193f3af4bd08b9e
Karan Chaudhary <tharukaran930@gmail.com>
2024-04-30 11:50:51 +0545

    fisrt-app

--- app/(tabs)/three.tsx
+++ app/(tabs)/three.tsx
@@ -1,12 +1,12 @@
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 
-export default function TabTwoScreen() {
+export default function TabThreeScreen() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-xl font-bold">Tab Two</Text>
+      <Text className="text-xl font-bold">Tab Three</Text>
       <View className="my-6 h-[1px] bg-gray-300 dark:bg-gray-800 w-4/5" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <EditScreenInfo path="app/(tabs)/three.tsx" />
     </View>
   );
 }
--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,12 +1,11 @@
-import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
+import React from "react";
 
-export default function TabTwoScreen() {
+export default function MainScreen() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-xl font-bold">Tab Two</Text>
+      <Text className="text-xl font-bold">Home Page</Text>
       <View className="my-6 h-[1px] bg-gray-300 dark:bg-gray-800 w-4/5" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
 }
