087d634b41ac43283db67929cb61c4d68f308d0c
Karan Chaudhary <tharukaran930@gmail.com>
2024-04-30 17:02:33 +0545

    changes

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import ActivityScreen from "@/screen/activity";
 import HomeScreen from "@/screen/home";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -8,7 +9,16 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Activity"
+          component={ActivityScreen}
+          options={{ headerShown: false }}
+        />
+        {/* <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: true }}
+        /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
